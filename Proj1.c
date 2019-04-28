bafaf17b88fe0db84b74df20ee4c19dc75a9e812
Jayden Maidstone <jaydensmail@internode.on.net>
2019-04-28 09:56:56 +0000

    sub encrypt

--- Proj1.c
+++ Proj1.c
@@ -11,15 +11,15 @@ void decryptSA();
 int main(void)
 {
     int option;
-    printf("Select an option from 1 to 6\n 1.  Encryption with rotation cipher given message text and rotation amount\n 2.  Decryption with rotation cipher given cipher text and rotation amount\n 3.  Encryption with a substitution cipher given message text and alphabet substitution\n 4.  Decryption of a message encrypted with a substitution cipher given cipher text and substitutions\n 5.  Decryption of a message encrypted with a rotation cipher given cipher text only\n 6.  Decryption of a message encrypted with a substitution cipher given cipher text only" );
+    printf("Select an option from 1 to 6\n 1.  Encryption with rotation cipher given message text and rotation amount\n 2.  Decryption with rotation cipher given cipher text and rotation amount\n 3.  Encryption with a substitution cipher given message text and alphabet substitution\n 4.  Decryption of a message encrypted with a substitution cipher given cipher text and substitutions\n 5.  Decryption of a message encrypted with a rotation cipher given cipher text only\n 6.  Decryption of a message encrypted with a substitution cipher given cipher text only\n" );
     scanf("%d", &option);
     switch(option) {
         case 1: encryptR();
         break;
         case 2: decryptR();
         break;
-//        case 3: encryptS();
-//        break;
+        case 3: encryptS();
+        break;
 //        case 4: decryptS();
 //        break;
         case 5: decryptRA();
@@ -96,4 +96,33 @@ void decryptR() {
    key += 1;
    }
    
+}
+
+
+
+void encryptS() {
+    
+    FILE *ofile;
+    FILE *key;
+    int count = 0;
+    ofile = fopen("output.txt", "w");
+    key = fopen("key.txt", "r");
+   FILE *input;
+   char x;
+   char k;
+   input = fopen("encrypt_input.txt", "r");
+   while ((x = getc(input)) != EOF) {
+       if (x >= 65 && x <= 90) {
+       while (count != (x - 64)) {
+          k = getc(key);
+          count += 1;
+      }
+    x = k;
+        }
+    fprintf( ofile,"%c", x);
+    count = 0;
+    fseek(key, 0, SEEK_SET);  
+   }
+   
+   fclose(ofile);
 }
